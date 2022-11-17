1c0fa0d2108bc39b7e16c6a8e8445d8f376c2891
Kim-Yong-Soo <dydtnalswl@gmail.com>
2022-11-17 14:32:35 +0900

    Add SQlite CRUD

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:draggable_home/draggable_home.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 void main() {
@@ -37,7 +42,7 @@ class MyStatefulWid extends StatelessWidget {
       appBarColor: Colors.blueAccent,
       body: [
         Padding(
-          padding: EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
           child: Card(
             shape: const RoundedRectangleBorder(
               borderRadius: BorderRadius.all(
@@ -67,7 +72,7 @@ class MyStatefulWid extends StatelessWidget {
                   child: Ink(
                     decoration: BoxDecoration(
                       border:
-                      Border.all(color: Colors.lightBlueAccent, width: 1),
+                          Border.all(color: Colors.lightBlueAccent, width: 1),
                       borderRadius: BorderRadius.circular(50),
                     ),
                     child: InkWell(
@@ -121,12 +126,12 @@ class MyStatefulWid extends StatelessWidget {
                   borderRadius: BorderRadius.circular(100),
                   onTap: () {},
                   child: const Padding(
-                    padding: const EdgeInsets.all(12),
+                    padding: EdgeInsets.all(12),
                     child: Center(
                         child: Text(
-                          "일기 쓰기",
-                          style: TextStyle(color: Colors.white),
-                        )),
+                      "일기 쓰기",
+                      style: TextStyle(color: Colors.white),
+                    )),
                   ),
                 ),
               ),
@@ -220,29 +225,29 @@ Widget emotionDays(String emotion) {
                   child: GridView.builder(
                       scrollDirection: Axis.horizontal,
                       gridDelegate:
-                      const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 1,
-                          childAspectRatio: 2 / 2,
-                          crossAxisSpacing: 12,
-                          mainAxisSpacing: 12),
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 1,
+                              childAspectRatio: 2 / 2,
+                              crossAxisSpacing: 12,
+                              mainAxisSpacing: 12),
                       itemCount: 3,
                       itemBuilder: (context, index) => Card(
-                        margin: const EdgeInsets.all(8),
-                        elevation: 8,
-                        child: GridTile(
-                          footer: GridTileBar(
-                            backgroundColor: Colors.black38,
-                            title: const Text("footer"),
-                            subtitle: Text('Item ${index}'),
-                          ),
-                          child: Center(
-                            child: Text(
-                              index.toString(),
-                              style: const TextStyle(fontSize: 16),
+                            margin: const EdgeInsets.all(8),
+                            elevation: 8,
+                            child: GridTile(
+                              footer: GridTileBar(
+                                backgroundColor: Colors.black38,
+                                title: const Text("footer"),
+                                subtitle: Text('Item $index'),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  index.toString(),
+                                  style: const TextStyle(fontSize: 16),
+                                ),
+                              ),
                             ),
-                          ),
-                        ),
-                      )),
+                          )),
                 ),
               ],
             ),
@@ -251,4 +256,113 @@ Widget emotionDays(String emotion) {
       ],
     ),
   );
+}
+
+class Dialog {
+  final int? id, year, month, day;
+  final String? content, emotion;
+
+  Dialog(
+      {this.id, this.year, this.month, this.day, this.content, this.emotion});
+
+  Map<String, dynamic> toMap() => {
+        'id': this.id,
+        'year': this.year,
+        'month': this.month,
+        'day': this.day,
+        'content': this.content,
+        'emotion': this.emotion,
+      };
+}
+
+const String TableName = 'Dialog';
+
+class DBHelper {
+  DBHelper._();
+
+  static final DBHelper _db = DBHelper._();
+
+  factory DBHelper() => _db;
+
+  late Database _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database;
+
+    _database = await initDB();
+    return _database;
+  }
+
+  initDB() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, 'MyDialogDB.db');
+
+    return await openDatabase(path, version: 1, onCreate: (db, version) async {
+      await db.execute('''
+          CREATE TABLE $TableName(
+            id INTEGER PRIMARY KEY,
+            year INTEGER,
+            month INTEGER,
+            day INTEGER,
+            content TEXT,
+            emotion TEXT,
+          )
+        ''');
+    }, onUpgrade: (db, oldVersion, newVersion) {});
+  }
+
+  //Create
+  createData(Dialog dialog) async {
+    final db = await database;
+    var res = await db.rawInsert('INSERT INTO $TableName(name) VALUES(?)', [
+      dialog.year,
+      dialog.month,
+      dialog.day,
+      dialog.content,
+      dialog.emotion
+    ]);
+    return res;
+  }
+
+  //Read
+  getDialog(int id) async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = (await db.query(
+      'Dialog',
+      where: 'id = ?',
+      whereArgs: [id],
+    ));
+
+    return maps.isNotEmpty ? maps : null;
+  }
+
+  //Read All
+  Future<List<Dialog>> getAllDialog() async {
+    final db = await database;
+
+    final List<Map<String, dynamic>> maps = await db.query('Dog');
+
+    return List.generate(maps.length, (i) {
+      return Dialog(
+          id: maps[i]['id'],
+          year: maps[i]['year'],
+          month: maps[i]['month'],
+          day: maps[i]['day'],
+          content: maps[i]['content'],
+          emotion: maps[i]['emotion']);
+    });
+  }
+
+  //Delete
+  deleteDialog(int id) async {
+    final db = await database;
+    var res = db.rawDelete('DELETE FROM $TableName WHERE id = ?', [id]);
+    return res;
+  }
+
+  //Delete All
+  deleteAllDialogs() async {
+    final db = await database;
+    db.rawDelete('DELETE FROM $TableName');
+  }
 }
